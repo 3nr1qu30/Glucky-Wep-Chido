7c33cb9476c79ab7540464236593e2c06b9da964
3nr1qu30 <94588401+3nr1qu30@users.noreply.github.com>
2023-03-11 16:05:54 -0600

    Add files via upload

--- src/routers/customer.js
+++ src/routers/customer.js
@@ -7,5 +7,6 @@ router.get('/', customerController.index);
 router.get('/Registro', customerController.registros);
 router.get('/Registro/Paciente',customerController.registroPaciente);
 router.get('/Registro/Doctor',customerController.registroDoctor);
-
+router.post('/Registro/Paciente',customerController.registroPacientePost);
+router.post('/Registro/Doctor', customerController.registroDoctorPost);
 module.exports = router;
